3c36f95d1568e3054cf0152133c02724fe3e4975
Shivram <sr31415@outlook.com>
2021-07-03 18:17:45 -0400

    Configure Tailwind screens

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo max-h-48" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		screens: {
+		  'xxs': '320px',
+		  'xs': '475px',
+		  ...defaultTheme.screens,
+		}
 	},
 	variants: {
 		extend: {},
