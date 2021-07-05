6f3126f06d4024582b31a0b092df5ed248e0b7df
Shivram <sr31415@outlook.com>
2021-07-05 16:40:23 -0400

    Modify navbar hover text color

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,15 +4,22 @@ function Nav() {
 	return (
 		<div
 			className='h-20 bg-gradient-to-r from-gray-700 to-gray-700
-            via-gray-900 text-white px-8 py-4 text-lg'
+            via-gray-900 px-8 py-4 text-lg'
 		>
 			<div
 				className='flex max-w-3xl justify-between items-center h-full
                 ml-auto mr-auto'
 			>
 				<img src={logo} className='h-full' alt='placeholdersvg' />
-				<div className='hidden xs:block'>Browse APIs</div>
-				<div>GitHub</div>
+				<p
+					className='text-white hover:text-gray-300 hidden xs:block
+                    cursor-pointer'
+				>
+					Browse APIs
+				</p>
+				<p className='text-white hover:text-gray-300 cursor-pointer'>
+					GitHub
+				</p>
 			</div>
 		</div>
 	);
