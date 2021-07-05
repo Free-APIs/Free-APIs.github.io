5cb5037368a3a4cc2b8bcff7514e61cda185efaa
Shivram <sr31415@outlook.com>
2021-07-05 12:12:42 -0400

    Remove unused header in App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
-import logo from './logo.svg';
 
 function App() {
 	return (
 		<div className='App flex flex-col h-screen justify-between'>
 			<Nav />
 			<Jumbotron />
-			<header className='App-header'>
-				<img src={logo} className='App-logo max-h-48' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
 			<Footer />
 		</div>
 	);
