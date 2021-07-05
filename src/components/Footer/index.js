803e1d1e84fc9cc5480706d2f278cea5d3c0cc42
Shivram <sr31415@outlook.com>
2021-07-05 16:36:21 -0400

    Modify footer text color and subtitle text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ function Footer() {
 			className='flex justify-center text-center items-center p-6
             bg-gradient-to-r from-gray-300 to-gray-300 via-gray-400'
 		>
-			<p className='text-gray-800 hover:text-gray-600 cursor-pointer'>
+			<p className='text-gray-900 hover:text-gray-700 cursor-pointer'>
 				About this project
 			</p>
 		</div>
--- src/components/Subtitle/index.js
+++ src/components/Subtitle/index.js
@@ -1,7 +1,7 @@
 function Subtitle() {
 	return (
 		<p className='mx-8 mb-6 text-gray-600 xs:text-lg text-center'>
-			A collection of Free APIs derived from Public APIS
+			A collection of free APIs derived from Public APIS
 		</p>
 	);
 }
