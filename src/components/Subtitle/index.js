4559fce3fb5af8bba463c303e67980408dc8d876
Shivram <sr31415@outlook.com>
2021-07-06 15:07:00 -0400

    Add link to Public-APIs repo

--- src/components/Subtitle/index.js
+++ src/components/Subtitle/index.js
@@ -1,7 +1,15 @@
 function Subtitle() {
 	return (
 		<p className='mx-8 mb-6 text-gray-600 xs:text-lg text-center'>
-			A collection of free APIs derived from Public APIs
+			A collection of free APIs derived from&nbsp;
+			<a
+				href='https://github.com/public-apis/public-apis'
+				rel='noreferrer'
+				target='_blank'
+				className='underline hover:text-gray-500'
+			>
+				Public APIs
+			</a>
 		</p>
 	);
 }
