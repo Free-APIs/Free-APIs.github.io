8a6fd4abc4b5a399c6cd8501ae71157cb2cb9d0f
Shivram <sr31415@outlook.com>
2021-07-05 20:36:45 -0400

    Change first button text

--- src/components/Jumbotron/ButtonRows/index.js
+++ src/components/Jumbotron/ButtonRows/index.js
@@ -3,7 +3,7 @@ import Button from '../../Button';
 function ButtonRows() {
 	return (
 		<div className='w-full flex justify-center items-center flex-wrap p-4'>
-			<Button>Browse to get inspired</Button>
+			<Button>Browse all APIs</Button>
 			<Button>Explore by category</Button>
 		</div>
 	);
