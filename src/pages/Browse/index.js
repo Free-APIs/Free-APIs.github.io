b21d5e7519152833d92c494024888058597413e6
Shivram <sr31415@outlook.com>
2021-07-06 16:47:08 -0400

    Add API fetch proof of concept

--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -1,6 +1,30 @@
 import Template from '../Template';
+import { useState, useEffect } from 'react';
 
 function Browse() {
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [info, setInfo] = useState({});
+
+	useEffect(() => {
+		setError(null);
+		setIsLoaded(false);
+		fetch('https://api.publicapis.org/entries')
+			.then((response) => response.json())
+			.then(
+				(data) => {
+					setInfo(data);
+					setIsLoaded(true);
+				},
+				(error) => {
+					setError(error);
+					setIsLoaded(true);
+				},
+			);
+	}, []);
+
+	console.log(info);
+
 	return <Template />;
 }
 
