cf7052b64d4c43f3e3ed613f36fa4896960eca92
Shivram <sr31415@outlook.com>
2021-07-07 19:08:41 -0400

    Make Browse component fetch APIs

--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -1,31 +1,65 @@
 import Template from '../Template';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+// import DataStore from '../../components/DataStore';
 
 function Browse() {
+	const [apis, setApis] = useState();
+	const [query, setQuery] = useState(0);
 	const [error, setError] = useState(null);
 	const [isLoaded, setIsLoaded] = useState(false);
-	const [info, setInfo] = useState({});
 
 	useEffect(() => {
 		setError(null);
 		setIsLoaded(false);
-		fetch('https://api.publicapis.org/entries')
-			.then((response) => response.json())
-			.then(
-				(data) => {
-					setInfo(data);
-					setIsLoaded(true);
-				},
-				(error) => {
-					setError(error);
-					setIsLoaded(true);
-				},
-			);
-	}, []);
 
-	console.log(info);
+		const cached = sessionStorage.getItem('data');
 
-	return <Template />;
+		if (!cached) {
+			fetch('https://api.publicapis.org/entries')
+				.then((response) => response.json())
+				.then(
+					(data) => {
+						onSetResult(data);
+						console.log('pulling');
+						setIsLoaded(true);
+					},
+					(error) => {
+						setError(error);
+						setIsLoaded(true);
+					},
+				);
+		} else {
+			setApis(JSON.stringify(cached));
+			setIsLoaded(true);
+			console.log('cache hit');
+		}
+	}, [query]);
+
+	const onSetResult = (result) => {
+		sessionStorage.setItem('data', JSON.stringify(result));
+		setApis(result);
+		console.log(result);
+	};
+
+	const output = () => {
+		if (error) {
+			return 'Error occurred';
+		} else if (!isLoaded) {
+			return 'isLoading';
+		} else {
+			return apis;
+		}
+	};
+
+	console.log(output());
+
+	return (
+		<Template>
+			<div onClick={() => setQuery((c) => c + 1)}>
+				{JSON.stringify(output())}
+			</div>
+		</Template>
+	);
 }
 
 export default Browse;
