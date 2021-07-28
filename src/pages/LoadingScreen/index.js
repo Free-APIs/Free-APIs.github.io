374b7cb2cfb2a509ee6f8e0a23ad6598819149f0
Shivram <sr31415@outlook.com>
2021-07-28 14:38:05 -0400

    Make loading screen closer resemble cards

--- src/pages/LoadingScreen/index.js
+++ src/pages/LoadingScreen/index.js
@@ -3,7 +3,7 @@ import Template from '../Template';
 function LoadingScreen() {
 	const card = (
 		<div
-			className='w-72 h-64 m-4 p-4 bg-gray-200
+			className='w-72 h-64 m-3 p-4 bg-gray-200
             rounded-xl flex-none shadow-lg flex flex-col'
 		>
 			<div className='rounded-lg h-12 w-28 bg-gray-400 mb-4' />
@@ -25,7 +25,7 @@ function LoadingScreen() {
 					className='h-16 w-32 bg-gray-700 
                     rounded-xl m-4 shadow-lg'
 				/>
-				<div className='flex justify-center flex-wrap'>
+				<div className='flex justify-center flex-wrap p-4 pt-0'>
 					{card}
 					{card}
 					{card}
