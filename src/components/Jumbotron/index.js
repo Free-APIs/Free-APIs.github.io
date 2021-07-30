04e191922e3b0f7551c54e764be601889a81bfea
Shivram <sr31415@outlook.com>
2021-07-29 20:24:20 -0400

    Adjust Home page content spacing

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,26 +4,29 @@ import ButtonRows from './ButtonRows';
 
 function Jumbotron() {
 	return (
-		<div
-			className='h-auto bg-gray-300 flex flex-col justify-center
-            items-center flex-grow pb-6'
-		>
+		<>
 			<div
-				className='text-6xl xs:text-7xl sm:text-8xl font-bold
-                text-center mb-6 mt-12'
+				className='h-auto bg-gray-300 flex flex-col justify-center
+                items-center pb-6'
 			>
-				Free APIs
+				<div
+					className='text-6xl xs:text-7xl sm:text-8xl font-bold
+                    text-center mb-6 mt-12'
+				>
+					Free APIs
+				</div>
+				<Subtitle />
+				<div className='mx-6'>
+					<img
+						src={page}
+						className='max-h-96 rounded-lg shadow-xl'
+						alt='Development APIs'
+					/>
+				</div>
+				<ButtonRows />
 			</div>
-			<Subtitle />
-			<div className='mx-6'>
-				<img
-					src={page}
-					className='max-h-96 rounded-lg shadow-xl'
-					alt='Development APIs'
-				/>
-			</div>
-			<ButtonRows />
-		</div>
+			<div className='flex-grow max-h-10' />
+		</>
 	);
 }
 
