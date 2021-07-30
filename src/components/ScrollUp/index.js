912e7a1191d71ff9e59340c7643682910302af25
Shivram <sr31415@outlook.com>
2021-07-29 22:03:47 -0400

    Move scrollUp on browse higher to work more on Safari
    Still necessary occasionally to press multiple times if lower navBar is in view.

--- src/components/ScrollUp/index.js
+++ src/components/ScrollUp/index.js
@@ -2,8 +2,8 @@ function ScrollUp() {
 	return (
 		<div
 			className='rounded-full h-12 w-12 text-gray-200 bg-gradient-to-r
-            from-gray-700 to-gray-800 fixed bottom-0 right-0 m-8 text-lg 
-            cursor-pointer flex justify-center items-center shadow-lg
+            from-gray-700 to-gray-800 fixed xs:bottom-0 right-0 m-8 text-lg 
+            cursor-pointer flex justify-center items-center shadow-lg bottom-12
             hover:shadow-xl hover:from-gray-600'
 			onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
 		>
