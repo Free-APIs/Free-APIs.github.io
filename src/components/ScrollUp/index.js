d0054b6b3fb34b7002edc942771b5f9c062468b5
Shivram <sr31415@outlook.com>
2021-07-31 19:56:47 -0400

    Increase scrollUp button base height

--- src/components/ScrollUp/index.js
+++ src/components/ScrollUp/index.js
@@ -21,7 +21,7 @@ function ScrollUp() {
 		<div
 			className='rounded-full h-12 w-12 text-gray-200 bg-gradient-to-r
             from-gray-700 to-gray-800 fixed xs:bottom-0 right-0 m-8 text-lg 
-            cursor-pointer flex justify-center items-center shadow-lg bottom-6
+            cursor-pointer flex justify-center items-center shadow-lg bottom-10
             hover:shadow-xl hover:from-gray-600'
 			onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
 		>
