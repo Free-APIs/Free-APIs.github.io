6283bd1a535d2abb0cb7c9b74c7fe832d699b30d
Free-APIs <sr31415+free-apis@outlook.com>
2022-02-19 15:39:39 -0500

    Fix Category useEffect return

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -6,161 +6,165 @@ import { Link } from 'react-router-dom';
 import ScrollUp from '../../components/ScrollUp';
 
 function Category() {
-	const [apis, setApis] = useState([]);
-	const [displayList, setDisplayList] = useState([]);
-	const [searchText, setSearchText] = useState('');
-	const [error, setError] = useState(null);
-	const [refresh, setRefresh] = useState(false);
-	const [backup, setBackup] = useState();
-	const [canStore, setCanStore] = useState();
+  const [apis, setApis] = useState([]);
+  const [displayList, setDisplayList] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [error, setError] = useState(null);
+  const [refresh, setRefresh] = useState(false);
+  const [backup, setBackup] = useState();
+  const [canStore, setCanStore] = useState();
 
-	const category = useParams();
+  const category = useParams();
 
-	useEffect(() => {
-		document.title = `${category['category']} - Free APIs`;
-	});
+  useEffect(() => {
+    document.title = `${category['category']} - Free APIs`;
+  });
 
-	useEffect(() => {
-		try {
-			let storage = window['sessionStorage'];
-			let x = '__storage_test__';
-			storage.setItem(x, x);
-			storage.removeItem(x);
-			setCanStore(true);
-		} catch (e) {
-			setCanStore(false);
-		}
-	}, []);
+  useEffect(() => {
+    try {
+      let storage = window['sessionStorage'];
+      let x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      setCanStore(true);
+    } catch (e) {
+      setCanStore(false);
+    }
+  }, []);
 
-	useEffect(() => {
-		setError(null);
+  useEffect(() => {
+    setError(null);
 
-		let stored;
+    let stored;
 
-		if (canStore) {
-			stored = sessionStorage.getItem('data');
-		} else {
-			stored = backup;
-		}
+    if (canStore) {
+      stored = sessionStorage.getItem('data');
+    } else {
+      stored = backup;
+    }
 
-		if (!stored || refresh) {
-			fetch('https://api.publicapis.org/entries')
-				.then((response) => response.json())
-				.then(
-					(data) => {
-						if (canStore) {
-							onSetResult(data);
-						} else {
-							onSetResultBackup(data);
-						}
-					},
-					(error) => {
-						setError(error);
-					},
-				);
-			setRefresh(false);
-		} else {
-			if (canStore) {
-				setApis(JSON.parse(stored));
-			} else {
-				setApis(stored);
-			}
-		}
-	}, [refresh, canStore, backup]);
+    if (!stored || refresh) {
+      fetch('https://api.publicapis.org/entries')
+        .then((response) => response.json())
+        .then(
+          (data) => {
+            if (canStore) {
+              onSetResult(data);
+            } else {
+              onSetResultBackup(data);
+            }
+          },
+          (error) => {
+            setError(error);
+          },
+        );
+      setRefresh(false);
+    } else {
+      if (canStore) {
+        setApis(JSON.parse(stored));
+      } else {
+        setApis(stored);
+      }
+    }
 
-	const onSetResult = (result) => {
-		let value = result['entries'];
-		const unique = [
-			...new Map(value.map((api) => [api['API'], api])).values(),
-		];
-		const uniqueString = JSON.stringify(unique);
-		sessionStorage.setItem('data', uniqueString);
-		setApis(unique);
-	};
+    return () => {
+      setApis([]);
+    }
+  }, [refresh, canStore, backup]);
 
-	const onSetResultBackup = (result) => {
-		let value = result['entries'];
-		const unique = [
-			...new Map(value.map((api) => [api['API'], api])).values(),
-		];
-		setBackup(unique);
-		setApis(unique);
-	};
+  const onSetResult = (result) => {
+    let value = result['entries'];
+    const unique = [
+      ...new Map(value.map((api) => [api['API'], api])).values(),
+    ];
+    const uniqueString = JSON.stringify(unique);
+    sessionStorage.setItem('data', uniqueString);
+    setApis(unique);
+  };
 
-	const shuffle = () => {
-		let shuffled = [...apis];
-		for (let i = shuffled.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-		}
-		setApis([...shuffled]);
-	};
+  const onSetResultBackup = (result) => {
+    let value = result['entries'];
+    const unique = [
+      ...new Map(value.map((api) => [api['API'], api])).values(),
+    ];
+    setBackup(unique);
+    setApis(unique);
+  };
 
-	const reset = () => {
-		if (canStore) {
-			setApis(JSON.parse(sessionStorage.getItem('data')));
-		} else {
-			setApis(backup);
-		}
-	};
+  const shuffle = () => {
+    let shuffled = [...apis];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    setApis([...shuffled]);
+  };
 
-	useEffect(() => {
-		const matches = (list) => {
-			return (
-				list['Category'] === category['category'] &&
-				(list['API'].toLowerCase().includes(searchText.toLowerCase()) ||
-					list['Description']
-						.toLowerCase()
-						.includes(searchText.toLowerCase()))
-			);
-		};
-		setDisplayList(apis.filter(matches));
-	}, [searchText, apis, category]);
+  const reset = () => {
+    if (canStore) {
+      setApis(JSON.parse(sessionStorage.getItem('data')));
+    } else {
+      setApis(backup);
+    }
+  };
 
-	const searchBar = (
-		<input
-			placeholder='Search APIs'
-			onChange={(e) => setSearchText(e.target.value)}
-			value={searchText}
-			className='px-4 my-4 outline-none focus:ring rounded-lg
+  useEffect(() => {
+    const matches = (list) => {
+      return (
+        list['Category'] === category['category'] &&
+        (list['API'].toLowerCase().includes(searchText.toLowerCase()) ||
+          list['Description']
+            .toLowerCase()
+            .includes(searchText.toLowerCase()))
+      );
+    };
+    setDisplayList(apis.filter(matches));
+  }, [searchText, apis, category]);
+
+  const searchBar = (
+    <input
+      placeholder='Search APIs'
+      onChange={(e) => setSearchText(e.target.value)}
+      value={searchText}
+      className='px-4 my-4 outline-none focus:ring rounded-lg
             hover:shadow focus:shadow w-32 h-10 text-sm md:text-base mx-2'
-		/>
-	);
+    />
+  );
 
-	const handleRefresh = () => {
-		setRefresh(true);
-		setSearchText('');
-		reset();
-	};
+  const handleRefresh = () => {
+    setRefresh(true);
+    setSearchText('');
+    reset();
+  };
 
-	if (error) {
-		return 'Error occurred';
-	} else {
-		return (
-			<Template>
-				<ListDisplay
-					shuffle={shuffle}
-					reset={reset}
-					search={searchBar}
-					isCategory
-					numResults={displayList.length}
-					refresh={handleRefresh}
-				>
-					{displayList}
-				</ListDisplay>
-				<Link className='flex justify-center text-xl' to='/categories'>
-					<div
-						className='bg-gray-300 shadow-md rounded-lg
+  if (error) {
+    return 'Error occurred';
+  } else {
+    return (
+      <Template>
+        <ListDisplay
+          shuffle={shuffle}
+          reset={reset}
+          search={searchBar}
+          isCategory
+          numResults={displayList.length}
+          refresh={handleRefresh}
+        >
+          {displayList}
+        </ListDisplay>
+        <Link className='flex justify-center text-xl' to='/categories'>
+          <div
+            className='bg-gray-300 shadow-md rounded-lg
                         p-3 mx-4 mt-0 mb-8 cursor-pointer hover:shadow-lg
                         hover:bg-gray-200'
-					>
-						&larr; Back to categories
-					</div>
-				</Link>
-				<ScrollUp />
-			</Template>
-		);
-	}
+          >
+            &larr; Back to categories
+          </div>
+        </Link>
+        <ScrollUp />
+      </Template>
+    );
+  }
 }
 
 export default Category;
